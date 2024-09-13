10fa321f0e965f571eee2dfc9212fba0823eda11
Metin Kuran <metinkuran98@gmail.com>
2024-09-13 22:48:14 +0300

    sharp2

--- server.js
+++ server.js
@@ -3,14 +3,20 @@ console.log('1111111111111111');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+console.log('222222222222222222222222');
+
 process.on('uncaughtException', (err) => {
   console.log('UNCAUGHT EXCEPTION! Shutting down...');
   console.log(err.name, err.message);
   process.exit(1);
 });
+console.log('333333333333333333333');
 
 dotenv.config({ path: './config.env' });
+console.log('4444444444444444444444');
+
 const app = require('./app');
+console.log('55555555555555555555555555');
 
 console.log('process.env.DATABASE: ', process.env.DATABASE);
 console.log('process.env.DATABASE_PASSWORD:: ', process.env.DATABASE_PASSWORD);
